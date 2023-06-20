4c99e4ba7ccb6983cda2d6df75a0dc4734feba93
CSMahajan <chaitanya.mahajan13@gmail.com>
2023-06-20 17:01:28 +0530

    Initial Commit

--- src/InsertionSortedArray.java
+++ src/InsertionSortedArray.java
@@ -11,35 +11,20 @@ public class InsertionSortedArray {
 
     public static int searchInsert(int[] arr, int m) {
         // Write your code here.
+        int l=0;
         int n = arr.length;
-        if(m >= arr[n - 1]) {
-            return n;
-        }
-        if(m <= arr[0]) {
-            return 0;
-        }
-        int correctPosition = 0, start = 0, end = n - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (arr[mid] <= m) {
-                //below if condition is add in case if that element m is already present in an array
-                if(arr[mid]==m)  {
-                    return mid;
-                }
-                //below part is added to check for arr[mid] < m
-                correctPosition = mid + 1;
-                //below if condition added to check if next element after mid should be greater or equal than m
-                //then it is confirmed that we found the correct position, so break is added
-                if(m <= arr[mid+1]) {
-                    break;
-                }
-                //otherwise we are continuing with binary search
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+        int h = n-1;
+        int ans = n;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(arr[mid]>=m){
+                ans=mid;
+                h=mid-1;
+            }else{
+                l=mid+1;
             }
         }
-        return correctPosition;
+        return ans;
     }
 
     public static void main(String[] args) {
