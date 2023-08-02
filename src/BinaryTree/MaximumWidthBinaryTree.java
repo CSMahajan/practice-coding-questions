8e09ca7d924701a916959c9f2dcfb667112391d7
CSMahajan <chaitanya.mahajan13@gmail.com>
2023-08-02 16:09:48 +0530

    Initial Commit

--- src/BinaryTree/MaximumWidthBinaryTree.java
+++ src/BinaryTree/MaximumWidthBinaryTree.java
@@ -6,38 +6,24 @@ import java.util.Queue;
 /*
 Maximum Width of Binary Tree
 
-Given a Binary Tree, find the maximum width of it.
-Maximum width is defined as the maximum number of nodes at any level.
-For example, the maximum width of the following tree is 4 as there are 4 nodes at the 3rd level.
-
-          1
-       /     \
-     2        3
-   /    \    /    \
-  4    5   6    7
-    \
-      8
-
+Given the root of a binary tree, return the maximum width of the given tree.
+The maximum width of a tree is the maximum width among all levels.
+The width of one level is defined as the length between the end-nodes (the leftmost and rightmost non-null nodes),
+where the null nodes between the end-nodes that would be present in a complete binary tree
+extending down to that level are also counted into the length calculation.
+It is guaranteed that the answer will in the range of a 32-bit signed integer.
 Example 1:
-Input:
-       1
-     /    \
-    2      3
-Output: 2
-On the first level there is only one node 1
-On the second level there are two nodes 2, 3 clearly it is the maximum number of nodes at any level
+Input: root = [1,3,2,5,3,null,9]
+Output: 4
+Explanation: The maximum width exists in the third level with length 4 (5,3,null,9).
 Example 2:
-Input:
-        10
-      /     \
-    20      30
-   /    \
-  40    60
+Input: root = [1,3,2,5,null,null,9,6,null,7]
+Output: 7
+Explanation: The maximum width exists in the fourth level with length 7 (6,null,null,null,null,null,7).
+Example 3:
+Input: root = [1,3,2,5]
 Output: 2
-There is one node on level 1(10)
-There is two node on level 2(20, 30)
-There is two node on level 3(40, 60)
-Hence the answer is 2
+Explanation: The maximum width exists in the second level with length 2 (3,2).
 */
 public class MaximumWidthBinaryTree {
 
