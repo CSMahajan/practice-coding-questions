f4fd4784dc2e5815fc8a35d849185aaeba638730
CSMahajan <chaitanya.mahajan13@gmail.com>
2023-08-04 13:49:54 +0530

    Initial Commit

--- src/BinarySearchTree/CheckBSTFromInorderTraversal.java
+++ src/BinarySearchTree/CheckBSTFromInorderTraversal.java
@@ -2,6 +2,18 @@ package BinarySearchTree;
 
 public class CheckBSTFromInorderTraversal {
 
+    static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
     public static boolean isValidBST(int[] order) {
         // Write your code here.
         for (int i = 0; i < order.length - 1; i++) {
@@ -13,7 +25,14 @@ public class CheckBSTFromInorderTraversal {
     }
 
     public static void main(String[] args) {
-        int[] inorder = {1, 3, 4, 5, 7, 8, 9};
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
+        int[] inorder = {1, 3, 5, 6, 7, 8, 9};
         System.out.println(isValidBST(inorder));
     }
 }
