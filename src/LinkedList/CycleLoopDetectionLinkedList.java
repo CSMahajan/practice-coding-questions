eb20c603a516e6d2513bc3c56aa7c14c2e89d021
CSMahajan <chaitanya.mahajan13@gmail.com>
2023-08-09 21:58:42 +0530

    Initial Commit

---
+++ src/LinkedList/CycleLoopDetectionLinkedList.java
@@ -0,0 +1,61 @@
+package LinkedList;
+
+/*
+Detect Loop in linked list
+
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+There is a cycle in a linked list if there is some node in the list that
+can be reached again by continuously following the next pointer.
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to.
+Note that pos is not passed as a parameter.
+Return true if there is a cycle in the linked list. Otherwise, return false.
+Example 1:
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+Example 2:
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+*/
+
+public class CycleLoopDetectionLinkedList {
+
+    static class ListNode {
+        int data;
+        ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+        }
+    }
+
+    //TC:O(N)
+    //SC:O(1)
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        CycleLoopDetectionLinkedList cldll = new CycleLoopDetectionLinkedList();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = head.next.next;
+        System.out.println(cldll.hasCycle(head));
+    }
+}
