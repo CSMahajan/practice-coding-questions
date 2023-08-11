431f9c48659d89410271d8b29d537c932ad0a043
CSMahajan <chaitanya.mahajan13@gmail.com>
2023-08-11 22:48:03 +0530

    Initial Commit

--- src/Stack/Stack3.java
+++ src/Stack/Stack3.java
@@ -5,5 +5,63 @@ Implement stack using linked list
 */
 public class Stack3 {
 
+    private static class stackNode {
+        int data;
+        stackNode next;
 
+        stackNode(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    stackNode top;
+    int size;
+
+    Stack3() {
+        this.top = null;
+        this.size = 0;
+    }
+
+    public void stackPush(int x) {
+        stackNode element = new stackNode(x);
+        element.next = top;
+        top = element;
+        System.out.println("Element pushed");
+        size++;
+    }
+
+    public int stackPop() {
+        if (top == null) return -1;
+        int topData = top.data;
+        top = top.next;
+        return topData;
+    }
+
+    public int stackSize() {
+        return size;
+    }
+
+    public boolean stackIsEmpty() {
+        return top == null;
+    }
+
+    public void printStack() {
+        stackNode current = top;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Stack3 s = new Stack3();
+        s.stackPush(10);
+        s.stackPush(20);
+        s.printStack();
+        System.out.println("Element popped " + s.stackPop());
+        System.out.println("Stack size: " + s.stackSize());
+        System.out.println("Stack is empty or not: " + s.stackIsEmpty());
+    }
 }
