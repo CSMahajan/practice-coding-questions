7208647d569620fb1c3fa74619f7108b1272db1c
CSMahajan <chaitanya.mahajan13@gmail.com>
2023-08-11 12:02:03 +0530

    Initial Commit

--- src/LinkedList/CloneCopyLinkedListWithNextAndRandomPointer.java
+++ src/LinkedList/CloneCopyLinkedListWithNextAndRandomPointer.java
@@ -25,6 +25,8 @@ public class CloneCopyLinkedListWithNextAndRandomPointer {
 
         public ListNode(int data) {
             this.data = data;
+            next = null;
+            random = null;
         }
 
     }
@@ -34,34 +36,82 @@ public class CloneCopyLinkedListWithNextAndRandomPointer {
     //Space Complexity: O(1)
     //Reason: No extra data structure was used for computation.
     public ListNode copyRandomList(ListNode head) {
-        return null;
+        ListNode temp = head;
+        //Step 1: Create a copy nodes linked list
+        //where new nodes are inserted in between original linked list with pointing of next pointers
+        while (temp != null) {
+            ListNode newNode = new ListNode(temp.data);
+            newNode.next = temp.next;
+            temp.next = newNode;
+            temp = temp.next.next;
+        }
+        //Step 2: Create random pointer pointing in deep copy similar to original linked list random pointer pointing
+        ListNode iter = head;
+        while (iter != null) {
+            if (iter.random != null) {
+                iter.next.random = iter.random.next;
+            }
+            iter = iter.next.next;
+        }
+        //Step 3: Make original linked list and deep copy linked list pointing correct by removing pointing between original and deep copy
+        ListNode dummy = new ListNode(0);
+        //here iter will move on to original list
+        //temp will move on to deep copy linked list
+        iter = head;
+        temp = dummy;
+        ListNode fast;
+        while (iter != null) {
+            fast = iter.next.next;
+            temp.next = iter.next;
+            iter.next = fast;
+            temp = temp.next;
+            iter = fast;
+        }
+        return dummy.next;
     }
 
     public void displayLinkedList(ListNode head) {
         while (head != null) {
-            System.out.print(head.data + "->");
+            System.out.print(head.data + ":");
+            if (head.next != null) {
+                System.out.print(head.next.data);
+            } else {
+                System.out.print("NULL");
+            }
+            if (head.random != null) {
+                System.out.print("," + head.random.data);
+            } else {
+                System.out.print(",NULL");
+            }
+            System.out.println();
             head = head.next;
         }
-        System.out.print("null");
     }
 
     public static void main(String[] args) {
         CloneCopyLinkedListWithNextAndRandomPointer ccllnrp = new CloneCopyLinkedListWithNextAndRandomPointer();
-        ListNode head1 = new ListNode(7);
-        ListNode head2 = new ListNode(13);
-        ListNode head3 = new ListNode(11);
-        ListNode head4 = new ListNode(10);
-        ListNode head5 = new ListNode(1);
-        head1.random = null;
-        head2.random = head1;
-        head3.random = head5;
-        head4.random = head3;
-        head5.random = head2;
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode head = null;
+        head = node1;
+        head.next = node2;
+        head.next.next = node3;
+        head.next.next.next = node4;
+        head.random = node4;
+        head.next.random = node1;
+        head.next.next.random = null;
+        head.next.next.next.random = node2;
         System.out.println("Before cloning a linked list");
-        ccllnrp.displayLinkedList(head1);
-        ListNode flattenedHead = ccllnrp.copyRandomList(head1);
+        System.out.println("Original list:(current node:node pointed by next pointer, " +
+                "node pointed by random pointer)");
+        ccllnrp.displayLinkedList(head);
+        ListNode cloneCopyHead = ccllnrp.copyRandomList(head);
         System.out.println();
         System.out.println("After cloning a linked list");
-        ccllnrp.displayLinkedList(flattenedHead);
+        System.out.println("Copy list:(current node:node pointed by next pointer,node " +
+                "pointed by random pointer)");
+        ccllnrp.displayLinkedList(cloneCopyHead);
     }
 }
