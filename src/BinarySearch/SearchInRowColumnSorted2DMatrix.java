89883977cb9bd047634c91028c140a68ace80c44
CSMahajan <chaitanya.mahajan13@gmail.com>
2023-08-19 00:20:09 +0530

    Initial Commit

--- src/BinarySearch/SearchInRowColumnSorted2DMatrix.java
+++ src/BinarySearch/SearchInRowColumnSorted2DMatrix.java
@@ -1,5 +1,50 @@
 package BinarySearch;
 
+/*
+Search in a row-column sorted Matrix
 
+Write an efficient algorithm that searches for a value target in an m x n integer matrix.
+This matrix has the following properties:
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+Example 1:
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+Output: true
+Example 2:
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+Output: false
+*/
 public class SearchInRowColumnSorted2DMatrix {
+
+    //Time complexity: O(n + m))
+    //Space complexity: O(1)
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int row = 0;
+        int column = m - 1;
+        while (row < n && column >= 0) {
+            if (matrix[row][column] == target) {
+                return true;
+            } else if (matrix[row][column] < target) {
+                row++;
+            } else {
+                column--;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 4, 7, 11, 15},
+                {2, 5, 8, 12, 19},
+                {3, 6, 9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}
+        };
+        int target = 18;
+        SearchInRowColumnSorted2DMatrix sircs2dm = new SearchInRowColumnSorted2DMatrix();
+        System.out.println(sircs2dm.searchMatrix(matrix, target));
+    }
 }
