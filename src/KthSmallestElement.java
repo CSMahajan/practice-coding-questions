4a37d296fe41aaa669bd029c201fbe2f07eb10a4
CSMahajan <chaitanya.mahajan13@gmail.com>
2023-12-20 15:23:47 +0530

    Initial Commit

--- src/KthSmallestElement.java
+++ src/KthSmallestElement.java
@@ -4,10 +4,10 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class KthSmallestElement {
-    public static int kthSmallest(int[] arr, int l, int r, int k)
+    public static int kthSmallest(int[] arr, int k)
     {
         //Your code here
-        Set<Integer> s = new TreeSet<Integer>();
+        Set<Integer> s = new TreeSet<>();
         for(int n:arr){
             s.add(n);
         }
@@ -17,13 +17,13 @@ public class KthSmallestElement {
             i.next();
             k--;
         }
-        return i.next().intValue();
+        return i.next();
         //Arrays.sort(arr);
         //return arr[k-1];
     }
 
     public static void main(String[] args) {
-        int arr[] = {7,10,4,3,20,15};
-        System.out.println(kthSmallest(arr,0,0, 3));
+        int[] arr = {7,10,4,3,20,15};
+        System.out.println(kthSmallest(arr, 3));
     }
 }
