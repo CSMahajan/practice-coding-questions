ab907ba50efe12053f51e174d2c7945cd42144f2
CSMahajan <chaitanya.mahajan13@gmail.com>
2023-12-21 23:37:10 +0530

    Initial Commit

--- src/LinkedList/Sort012LinkedList.java
+++ src/LinkedList/Sort012LinkedList.java
@@ -40,7 +40,33 @@ public class Sort012LinkedList {
     //Function to sort a linked list of 0s, 1s and 2s.
     public Node sort012LinkedList(Node head) {
         // add your code here
-        return head;
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node zeroHead = new Node(-1);
+        Node oneHead = new Node(-1);
+        Node twoHead = new Node(-1);
+        Node zero = zeroHead;
+        Node one = oneHead;
+        Node two = twoHead;
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data == 0) {
+                zero.next = temp;
+                zero = temp;
+            } else if (temp.data == 1) {
+                one.next = temp;
+                one = temp;
+            } else {
+                two.next = temp;
+                two = temp;
+            }
+            temp = temp.next;
+        }
+        zero.next = oneHead.next;
+        one.next = twoHead.next;
+        two.next = null;
+        return zeroHead.next;
     }
 
     public void displayLinkedList(Node head) {
@@ -54,11 +80,12 @@ public class Sort012LinkedList {
     public static void main(String[] args) {
         Sort012LinkedList sll = new Sort012LinkedList();
         Node head = new Node(2);
-        head.next = new Node(3);
-        head.next.next = new Node(9);
-        head.next.next.next = new Node(4);
-        head.next.next.next.next = new Node(1);
-        head.next.next.next.next.next = new Node(8);
+        head.next = new Node(1);
+        head.next.next = new Node(0);
+        head.next.next.next = new Node(2);
+        head.next.next.next.next = new Node(2);
+        head.next.next.next.next.next = new Node(1);
+        head.next.next.next.next.next.next = new Node(0);
         System.out.println("Linked List before 0 1 2 sorting");
         sll.displayLinkedList(head);
         Node sort012Head = sll.sort012LinkedList(head);
